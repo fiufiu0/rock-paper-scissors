7961ba81fcd3402fde7fff49a8fb20c88d0dd16e
Łukasz <nipseyyo@gmail.com>
2022-11-18 21:39:41 +0100

    Added computer choice function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("conected");
+
+function getComputerChoice() {
+    let choice = ["rock", "paper", "scissors"];
+    let random = choice[Math.floor(Math.random() * choice.length)];
+    console.log(`Computer: ${random}`);
+    return random;
+}
+
+getComputerChoice();
+
