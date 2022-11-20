274fc1ac26d37ffb9fbb0c69c249cecf838be778
Łukasz <nipseyyo@gmail.com>
2022-11-20 20:23:28 +0100

    Added game() function, 5 rounds

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"];
     let random = choice[Math.floor(Math.random() * choice.length)];
-    console.log(`Computer: ${random}`);
     return random;
 }
 
@@ -14,13 +13,23 @@ function playRound(playerSelection, computerSelection){
     (playerSelection == 'paper' && computerSelection == 'rock')){
         console.log(`Player win! ${playerSelection} vs ${computerSelection}` )
     }else if(playerSelection == computerSelection){
-        console.log("TIE")
+        console.log("Tie!")
     } else {
         console.log(`Computer win! ${computerSelection} vs ${playerSelection}`)
     }
 }
 
-const playerSelection = prompt("Rock, paper, scissors?").toLowerCase();
-const computerSelection = getComputerChoice();
 
-playRound(playerSelection, computerSelection);
+function game(){
+    
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, paper, scissors?").toLowerCase();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        
+     }
+
+    
+}
+
+game();
